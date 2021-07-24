55415c98b57c8235dc2fd0b5e0744206ebb76eb8
José Bezerra <josbezfi@gmail.com>
2021-07-24 20:42:46 -0300

    Refatorando - Criando dois pacotes novos (Acao e Modelo)

--- gerenciador/src/main/java/br/com/alura/gerenciador/acao/MostraEmpresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/acao/MostraEmpresa.java
@@ -1,23 +1,18 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
-@WebServlet("/mostraEmpresa")
-public class mostraEmpresaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+public class MostraEmpresa {
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Mostrando dados da empresa!");
 		String paramId = request.getParameter("id");
 		Integer id = Integer.valueOf(paramId);
 		
@@ -30,7 +25,5 @@ public class mostraEmpresaServlet extends HttpServlet {
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/formAlteraEmpresa.jsp");
 		rd.forward(request, response);
-		
 	}
-
 }
--- gerenciador/src/main/webapp/listaEmpresas.jsp
+++ gerenciador/src/main/webapp/listaEmpresas.jsp
@@ -24,8 +24,8 @@
 		<c:forEach items="${empresas}" var="empresa">
 			<li>
 			${empresa.nome} <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy"/> 
-			<a href="/gerenciador/mostraEmpresa?id=${empresa.id }">Edit</a>
-			<a href="/gerenciador/removeEmpresa?id=${empresa.id }">Remove</a>
+			<a href="/gerenciador/entrada?acao=MostraEmpresa&id=${empresa.id }">Edit</a>
+			<a href="/gerenciador/entrada?acao=RemoveEmpresa&id=${empresa.id }">Remove</a>
 			</li>
 		</c:forEach>
 		
